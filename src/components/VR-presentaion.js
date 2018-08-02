5673f891936532a44c6a4cd9cf6091c1fc1a5713
vli8 <vli8@fordham.edu>
2018-08-02 18:10:57 -0400

    walls built

--- src/components/VR-presentaion.js
+++ src/components/VR-presentaion.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Entity, Scene} from 'aframe-react';
 import {connect} from 'react-redux'
+import 'aframe-room-component'
+import Room from './Room'
 
 class VR extends React.Component {
     constructor(props) {
@@ -23,15 +25,22 @@ class VR extends React.Component {
             <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg"/>
             <img id = "wallGrey" src = "https://ucarecdn.com/9ad29c4d-2397-4ab9-8aaa-6f27152af272/" />
             <img id = "netflix" src = "https://ucarecdn.com/3de19143-0fd3-4a35-9e4d-e5ba031a194f/"/>
+            <img id = "starSky" src = "https://ucarecdn.com/1d9107f8-734c-48cc-b6e2-865af551a73a/"/>
+            <img id = "parquet" src = "https://ucarecdn.com/9662d212-6bb4-481f-b1b0-b666e1ccf65f/"/>
+            <audio id = "sound" src ="https://cdn.aframe.io/basic-guide/audio/backgroundnoise.wav" />
             </a-assets>
 
-            <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
+            <Entity primitive="a-plane" src="#parquet" rotation="-90 -1 0" height="100" width="100"/>
             <Entity primitive="a-light" type="ambient" color="#445451"/>
-            <Entity primitive="a-light" type="point" intensity="2" position="2 4 4"/>
-            <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
+            <Entity primitive="a-light" type="point" intensity=".5" position="2 4 4"/>
+            <Entity primitive = "a-sky" className="sky" src="#starSky" rotation="0 0 0"/>
             <Entity particle-system={{preset: 'snow', particleCount: 2000}}/>
             <Entity text={{value: `Hi ${this.props.name}!`, align: 'center', height : 6, width:6}} position={{x: 0, y: 2, z: -1}} />
-            <Entity primitive = 'a-cylinder' position={{x:1, y:0, z: 2}} src = "#netflix" value = "Hello World">
+
+            <Entity primitive= "a-sound" src= "#sound" autoplay = "true"/>
+
+
+            <Entity primitive = 'a-cylinder' position={{x:1, y:0, z: 2}} src = "#netflix">
                 <Entity text={{value: `Ben is helping`, align: 'center', height : 6, width:6}} position={{x: 0, y: 1, z: 0}} />
             </Entity> 
 
@@ -42,12 +51,14 @@ class VR extends React.Component {
             animation__rotate={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
             animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
             position={{x: 0, y: 1, z: -3}}
+            static body
             events={{click: this.changeColor.bind(this)}}>
                 <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
                         geometry={{primitive: 'box', depth: 0.2, height: 0.2, width: 0.2}}
                         material={{color: '#24CAFF'}}/>
             </Entity>
 
+                <Room />
             <Entity primitive="a-camera">
                 <Entity primitive="a-cursor" animation__click={{property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150}}/>
             </Entity>
